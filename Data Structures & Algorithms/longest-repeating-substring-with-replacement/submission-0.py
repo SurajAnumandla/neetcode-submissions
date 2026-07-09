4d2117a1391384115743a7090256cd2b735f86ee
Suraj Anumandla <101244128+SurajAnumandla@users.noreply.github.com>
2026-07-09 10:56:35 +0530

    Add: longest-repeating-substring-with-replacement - submission-0

---
+++ Data Structures & Algorithms/longest-repeating-substring-with-replacement/submission-0.py
@@ -0,0 +1,16 @@
+class Solution:
+    def characterReplacement(self, s: str, k: int) -> int:
+        n = len(s)
+        freq = {}
+        max_window = 0
+        max_freq = 0
+        left = 0
+        for right in range(n):
+            freq[s[right]] = freq.get(s[right],0)+1
+            max_freq = max(max_freq,freq[s[right]])
+
+            while (right-left+1) - max_freq > k:
+                freq[s[left]]-=1
+                left+=1
+            max_window = max(max_window,right-left+1)
+        return max_window
